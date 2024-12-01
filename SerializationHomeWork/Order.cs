191d5acd2731ef7b06a88472b5e4e23b94dee384
SarverCoder <sarvargamer@gmail.com>
2024-12-01 15:03:24 +0500

    Add project files.

---
+++ SerializationHomeWork/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationHomeWork
+{
+    internal class Order
+    {
+        public int OrderId { get; set; }
+        public string CostomerName { get; set; }
+        public double TotalAmount { get; set; }
+        public Status Status { get; set; }
+        public OrderItem orderItem { get; set; }
+
+    }
+
+    enum Status
+    {
+        Pending = 1,
+        Processing,
+        Shipped,
+        Delivered,
+        Cancelled,
+        Returned,
+        Failed
+
+
+    }
+}
---
+++ SerializationHomeWork/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace SerializationHomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new Order()
+            {
+                OrderId = 1,
+                CostomerName = "Michael",
+                TotalAmount = 23,
+                Status = Status.Pending,
+                orderItem = new OrderItem()
+                {
+                    ItemName = "VideoCard",
+                    Quantity = 10,
+                    Price = 450
+
+                }
+            };
+
+            var jsonOptions = new JsonSerializerOptions()
+            {
+                AllowTrailingCommas = false,
+                WriteIndented = true,
+                PropertyNameCaseInsensitive = true,
+            };
+
+           
+
+
+           // Console.WriteLine(text);
+
+            using var memoryStream = new MemoryStream();
+
+            var text = JsonSerializer.Serialize(order, jsonOptions);
+            var buffer = Encoding.UTF8.GetBytes(text);
+            memoryStream.Write(buffer, 0, buffer.Length);
+
+
+            string json = Encoding.UTF8.GetString(memoryStream.ToArray());
+            Console.WriteLine(json);
+            json = json.Replace("\"TotalAmount\": 23", "\"TotalAmount\": 10");
+
+
+            Console.WriteLine("\n\n");
+
+            var update = JsonSerializer.Deserialize<Order>(json);
+
+            Console.WriteLine("Update order object: ");
+
+            Console.WriteLine($" Id: {update.OrderId}\n Name: {update.CostomerName}\n Total: {update.TotalAmount}\n Status: {update.Status}\n" +
+                $"OrderItem : {update.orderItem.ItemName}");
+
+
+
+
+
+            
+
+        }
+    }
+}
